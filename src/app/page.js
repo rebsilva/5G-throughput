b9e86f8758c39c288300d60fbab75b77ab3fe390
Rebeca Silva <rebeca.silva@liferay.com>
2024-03-06 01:11:02 -0300

    create 5G throughput app

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,122 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client"; 
+import { useState} from 'react'
+import './index.css';
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+  const [parameters, setParameters] = useState('');
+  const [result, setResult] = useState('');
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+  let modulation_order = 0;
+  let bandwidth = 0;
+  let dl_ul = '';
+  let layers = 0;
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (!parameters) {
+      // do nothing
+    } 
+    else {
+      let inputParameters = parameters;
+      const myArray = inputParameters.split(',');
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+      // definir parâmetros
+      modulation_order = parseInt(myArray[0]);
+      bandwidth = parseInt(myArray[1]);
+      dl_ul = myArray[2];
+      layers = parseInt(myArray[3]);
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
+      // cálculo de um único recurso
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+      const subcarries = 12;
+      const data_symbols = 14 - 3;
+      const data_resource_elements = subcarries * data_symbols;
+
+      // mapeamento da tabela de eficiencia espectral
+
+      const spectral_efficiency_table = new Map();
+
+      spectral_efficiency_table.set(0,0.2344);
+      spectral_efficiency_table.set(1,0.377);
+      spectral_efficiency_table.set(2,0.6016);
+      spectral_efficiency_table.set(3,0.877);
+      spectral_efficiency_table.set(4,1.1758);
+      spectral_efficiency_table.set(5,1.4766);
+      spectral_efficiency_table.set(6,1.6953);
+      spectral_efficiency_table.set(7,1.9141);
+      spectral_efficiency_table.set(8,2.1602);
+      spectral_efficiency_table.set(9,2,4063);
+      spectral_efficiency_table.set(10,2.5703);
+      spectral_efficiency_table.set(11,2.7305);
+      spectral_efficiency_table.set(12,3.0293);
+      spectral_efficiency_table.set(13,3.3223);
+      spectral_efficiency_table.set(14,3.6094);
+      spectral_efficiency_table.set(15,3.9023);
+      spectral_efficiency_table.set(16,4.2129);
+      spectral_efficiency_table.set(17,4.5234);
+      spectral_efficiency_table.set(18,4.8164);
+      spectral_efficiency_table.set(19,5.1152);
+      spectral_efficiency_table.set(20,5.332);
+      spectral_efficiency_table.set(21,5.5547);
+      spectral_efficiency_table.set(22,5.8906);
+      spectral_efficiency_table.set(23,6.2266);
+      spectral_efficiency_table.set(24,6.5703);
+      spectral_efficiency_table.set(25,6.9141);
+      spectral_efficiency_table.set(26,7.1602);
+      spectral_efficiency_table.set(27,7.4063);
+
+      // definição do número máximo de bits
+
+      const max_bits_symbol = spectral_efficiency_table.get(modulation_order)
+
+      const slot_data = data_resource_elements * max_bits_symbol
+
+      // throughput
+
+      const number_resource_blocks = ((bandwidth*1000) / 360) - 4
+
+      const slots_per_sec = 1000/0.5
+
+      let downlink_slots = 0
+      if(dl_ul == '3:1') {
+        downlink_slots = slots_per_sec*(3/4);
+      } else if (dl_ul == '4:1') {
+        downlink_slots = slots_per_sec*(4/5)
+
+      } else {
+      alert('there is an error');
+      }
+
+
+      const throughput = slot_data * number_resource_blocks * downlink_slots * layers
+
+      // resultado
+
+      setResult(`the 5G throughput is ${throughput/1000000} mbps`)
+    }
+  }
+
+    return (
+      <>
+        <section className='section-center'>
+          <form className='form' onSubmit={handleSubmit}>
+            <h3>throughput 5G</h3>
+            <span className='description'>type the modulation order, bandwidth, download/upload and layers separated by comma.</span>
+            <div className='form-group'>
+              <input 
+                type='text' 
+                className='form-field input-parameters'
+                value={parameters}
+                placeholder='type the parameters'
+                onChange={(e)=> setParameters(e.target.value)}
+              />
+              <button type='submit' className='submit-btn'>
+                {'submit'}
+              </button>
+            </div>
+            <span>{result}</span>
+          </form>
+        </section>
+        </>
+      )
 }
